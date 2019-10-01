8be7c9008b6a5886aaff749e26facb85b0a4c349
Gabriel Grijalva Flores <gabriel.grijalva@sinstec.com>
2019-10-01 13:44:41 -0600

    Implemented okex order book.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 exports.BitmexOrderBook = require('./lib/src/bitmex/order-book');
 exports.HuobiDMOrderBook = require('./lib/src/huobi-dm/order-book');
 exports.BitflyerOrderBook = require('./lib/src/bitflyer/order-book');
+exports.OkexOrderBook = require('./lib/src/okex/order-book');
