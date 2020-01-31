a99e5cbc081d1b2c4daa3f55341aed040e7176f8
Gabriel Grijalva Flores <gabriel.grijalva@sinstec.com>
2020-01-30 18:27:13 -0700

    Implemented kraken order book.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 exports.BitmexOrderBook = require('./lib/src/bitmex/order-book');
 exports.HuobiDMOrderBook = require('./lib/src/huobi-dm/order-book');
+exports.KrakenOrderBook = require('./lib/src/kraken/order-book');
 exports.BitflyerOrderBook = require('./lib/src/bitflyer/order-book');
 exports.OkexOrderBook = require('./lib/src/okex/order-book');
